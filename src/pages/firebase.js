7e0212b6e8f3941e8aff1ce135255945a9d7d9d5
Rohit yadav <71065155+King9969@users.noreply.github.com>
2023-08-23 13:59:42 +0530

    Update firebase.js

--- src/pages/firebase.js
+++ src/pages/firebase.js
@@ -2,7 +2,14 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 import "firebase/compat/auth";
 const firebaseConfig = {
-
+apiKey: "AIzaSyDBicwW47LwJHq-TGzLHA8K1j5f3uviZIQ",
+  authDomain: "certain-benefit-236406.firebaseapp.com",
+  databaseURL: "https://certain-benefit-236406.firebaseio.com",
+  projectId: "certain-benefit-236406",
+  storageBucket: "certain-benefit-236406.appspot.com",
+  messagingSenderId: "813229051011",
+  appId: "1:813229051011:web:2a5cce6cf528c0598d9fb6",
+  measurementId: "G-QCQ8MZF8VT",
 };
 
 const app = firebase.initializeApp(firebaseConfig);
